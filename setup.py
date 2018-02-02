5566efbb23ff4a3dafc9fafe866f69fac3b30bba
mat-cor <mattia.cordioli01@universitadipavia.it>
2018-02-02 10:54:11 +0100

    Update add-on version

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ with io.open('about.md', 'r', encoding='utf-8') as f:
 NAME = "Orange3-Chemoinformatics"
 
 MAJOR = 0
-MINOR = 0
-MICRO = 2
+MINOR = 1
+MICRO = 0
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 AUTHOR = 'Bioinformatics Laboratory, FRI UL'
